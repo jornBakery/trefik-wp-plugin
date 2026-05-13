36b38bf3f11790a9a0aead20cbf173dad2248311
jorn_jhbdesigns <jorn@jhbdesigns.nl>
2026-05-13 15:45:58 +0200

    Updated image paths in email templates to use relative URLs for consistency and improved compatibility.

--- templates/emails/customer-completed-order-section-admin-order.php
+++ templates/emails/customer-completed-order-section-admin-order.php
@@ -429,7 +429,7 @@ if ( ! defined( 'ABSPATH' ) ) {
                                                                                                                             <td class="pc-w620-halign-right"
                                                                                                                                 align="right"
                                                                                                                                 valign="top">
-                                                                                                                                <img src="https://tref-ik.jhbdesigns.nl/wp-content/uploads/2025/04/icon-10.png"
+                                                                                                                                <img src="/wp-content/uploads/2025/04/icon-10.png"
                                                                                                                                     class="pc-w620-align-right"
                                                                                                                                     width="32"
                                                                                                                                     height="32"
--- templates/emails/customer-completed-order-section-contact.php
+++ templates/emails/customer-completed-order-section-contact.php
@@ -220,7 +220,7 @@ if ( ! defined( 'ABSPATH' ) ) {
                                                                                                                             <td class="pc-w620-halign-right"
                                                                                                                                 align="right"
                                                                                                                                 valign="top">
-                                                                                                                                <img src="https://tref-ik.jhbdesigns.nl/wp-content/uploads/2025/04/icon-9.png"
+                                                                                                                                <img src="/wp-content/uploads/2025/04/icon-9.png"
                                                                                                                                     class="pc-w620-align-right"
                                                                                                                                     width="32"
                                                                                                                                     height="25"
@@ -428,7 +428,7 @@ if ( ! defined( 'ABSPATH' ) ) {
                                                                                                                             <td class="pc-w620-halign-right"
                                                                                                                                 align="right"
                                                                                                                                 valign="top">
-                                                                                                                                <img src="https://tref-ik.jhbdesigns.nl/wp-content/uploads/2025/04/icon-10.png"
+                                                                                                                                <img src="/wp-content/uploads/2025/04/icon-10.png"
                                                                                                                                     class="pc-w620-align-right"
                                                                                                                                     width="32"
                                                                                                                                     height="32"
