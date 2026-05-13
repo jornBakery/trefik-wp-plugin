7d65df9773243bdea05b27cad8f033f579165c78
jorn_jhbdesigns <jorn@jhbdesigns.nl>
2026-05-13 15:44:43 +0200

    Updated image paths in email templates to use relative URLs for better compatibility.

--- templates/emails/customer-completed-order-section-admin-order.php
+++ templates/emails/customer-completed-order-section-admin-order.php
@@ -221,7 +221,7 @@ if ( ! defined( 'ABSPATH' ) ) {
                                                                                                                             <td class="pc-w620-halign-right"
                                                                                                                                 align="right"
                                                                                                                                 valign="top">
-                                                                                                                                <img src="https://tref-ik.jhbdesigns.nl/wp-content/uploads/2025/04/icon-9.png"
+                                                                                                                                <img src="/wp-content/uploads/2025/04/icon-9.png"
                                                                                                                                     class="pc-w620-align-right"
                                                                                                                                     width="32"
                                                                                                                                     height="25"
